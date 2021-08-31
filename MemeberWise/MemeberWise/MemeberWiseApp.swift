27237ce26943de0e8a17bbdc100beb7752c5f382
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-08-31 18:13:26 +0200

    Add initial project

---
+++ MemeberWise/MemeberWise/MemeberWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeberWiseApp.swift
+//  MemeberWise
+//
+//  Created by Cédric Bahirwe on 31/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemeberWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
