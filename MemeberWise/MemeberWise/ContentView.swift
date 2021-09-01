288c7496a90eb99deea08818bf1c87730f8e3e40
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-09-01 22:10:06 +0200

    Add Info

--- MemeberWise/MemeberWise/ContentView.swift
+++ MemeberWise/MemeberWise/ContentView.swift
@@ -9,17 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var emojis = ["🚂", "🚀", "🚁", "🚜", "🛺", "🚲", "🚐", "🛵", "🚘", "🛻", "🏎️", "🚙"]
-    
     @State private var emojiCount = 4
     
-    private var columns =  Array(repeating: GridItem(), count:4)
+    private let columns =  [GridItem(.adaptive(minimum: 70))]
     var body: some View {
         VStack {
             Text("MemeberWise")
                 .font(.largeTitle.bold())
             
             ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
+                LazyVGrid(columns: columns) {
                     ForEach(emojis[0..<emojiCount], id:\.self) { emoji in
                         CardView(emoji)
                             .aspectRatio(2/3,contentMode: .fit)
@@ -29,38 +28,46 @@ struct ContentView: View {
 
             Spacer()
             HStack {
-                Button(action: {
-                    withAnimation(.spring()) {
-                        if emojiCount > 1 {
-                            emojiCount -= 1
-                        }
-                    }
-                }, label: {
-                    Image(systemName: "minus.circle")
-                })
+                removeButton
                 Spacer()
                 Button("Shuffle") {
                     withAnimation {
                         emojis.shuffle()
                     }
                 }
+                .font(.system(.title, design: .monospaced))
                 Spacer()
-                Button(action: {
-                    withAnimation(.spring()) {
-                        if emojiCount < emojis.count {
-                            emojiCount += 1
-                        }
-                        
-                    }
-                }, label: {
-                    Image(systemName: "plus.circle")
-                })
+                addButton
+                
             }
             .font(.largeTitle)
             .padding(.horizontal)
         }
         .padding(.horizontal)
         
+    }
+    private var removeButton: some View {
+        Button(action: {
+            withAnimation(.spring()) {
+                if emojiCount < emojis.count {
+                    emojiCount += 1
+                }
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
+    }
+    
+    private var addButton: some View {
+        Button(action: {
+            withAnimation(.spring()) {
+                if emojiCount > 1 {
+                    emojiCount -= 1
+                }
+            }
+        }, label: {
+            Image(systemName: "minus.circle")
+        })
     }
 }
 
