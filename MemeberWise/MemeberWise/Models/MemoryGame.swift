d575a8b5186c00985abc6e1131985edf89bb951a
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-09-02 09:37:41 +0200

    Add Fonts

---
+++ MemeberWise/MemeberWise/Models/MemoryGame.swift
@@ -0,0 +1,23 @@
+//
+//  MemoryGame.swift
+//  MemeberWise
+//
+//  Created by Cédric Bahirwe on 02/09/2021.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: [Card]
+    
+    
+    func choose(card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
