8fb562e711ce0815c912306628ccc6069d44187b
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-09-08 16:38:55 +0200

    Add assets

---
+++ MemberWise/MemberWise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemberWise
+//
+//  Created by Cédric Bahirwe on 08/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemberWise/MemberWise/MemberWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemberWiseApp.swift
+//  MemberWise
+//
+//  Created by Cédric Bahirwe on 08/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemberWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
